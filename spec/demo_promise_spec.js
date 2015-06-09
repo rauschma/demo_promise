5bc48b7696b186aae08bcf0b28c3ebe8efbb5cc8
rauschma <axel@rauschma.de>
2015-06-09 20:25:58 +0200

    Explained jest.autoMockOff() hack.

--- spec/demo_promise_spec.js
+++ spec/demo_promise_spec.js
@@ -1,3 +1,6 @@
+// Imports are hoisted, which is why you can’t turn auto-mock off
+// via a method call in *this* module.
+// Work-around: do it in another module
 import './auto_mock_off';
 import { DemoPromise } from '../demo_promise4_exceptions';
 
